ca83df243e3e4f5dff66552ffb3cf231dbb39ac4
cowmuncher <cowmuncher@protonmail.com>
2026-08-20 02:23:07 +0200

    Add screenlock keybind

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -41,6 +41,7 @@ end)
 
 hl.env("XCURSOR_SIZE", "24")
 
+
 -- PERMISSIONS --
 
 
@@ -115,6 +116,7 @@ hl.config({
   },
 })
 
+
 -- MISC --
 
 
@@ -153,6 +155,7 @@ hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen({ action = "toggle"}))
 hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + B", hl.dsp.layout("togglesplit"))
+hl.bind(mainMod .. " + SHIFT + L", hl.dsp.exec_cmd("swaylock -f -c 000000 --indicator-thickness 5"))
 
 hl.bind(mainMod .. " + K",    hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + J",  hl.dsp.focus({ direction = "down" }))
